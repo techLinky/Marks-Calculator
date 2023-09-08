1cfa562d73c729f8cb101a4c1175b2fb4f0b091f
techLinky <gcebile.sne@gmail.com>
2023-09-08 12:32:13 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnResults = New Button()
+        Label1 = New Label()
+        txtMarks = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btnResults
+        ' 
+        btnResults.Location = New Point(165, 224)
+        btnResults.Name = "btnResults"
+        btnResults.Size = New Size(116, 57)
+        btnResults.TabIndex = 0
+        btnResults.Text = "Results"
+        btnResults.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(112, 87)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(105, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Enter marks"
+        ' 
+        ' txtMarks
+        ' 
+        txtMarks.Location = New Point(320, 87)
+        txtMarks.Name = "txtMarks"
+        txtMarks.Size = New Size(194, 31)
+        txtMarks.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtMarks)
+        Controls.Add(Label1)
+        Controls.Add(btnResults)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnResults As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtMarks As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub btbResults_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub btbResults_Click_1(sender As Object, e As EventArgs) Handles btnResults.Click
+        Dim Marks As Integer
+        If IsNumeric(txtMarks) = True Then
+
+        End If
+        MsgBox("Fail")
+        If Marks <= 50 Then
+
+        End If
+        MsgBox("Fail")
+
+        MsgBox("Invalid! ENTER A NUMBER")
+            If Marks < 0 Or Marks > 100 Then
+                MsgBox("Invalid mark")
+            ElseIf Marks > 50 Then
+                MsgBox("Pass")
+            End If
+
+
+    End Sub
+End Class
